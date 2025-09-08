aeb312913f8f3bec71d5ea55ad3bef78044a91fc
James Davies <77803980+jmdav@users.noreply.github.com>
2025-09-08 14:23:03 -0600

    test for vscode

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,10 @@
 import chess.*;
 
 public class Main {
-    public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
-    }
+  public static void main(String[] args) {
+    // git test
+    var piece =
+        new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+    System.out.println("♕ 240 Chess Client: " + piece);
+  }
 }
