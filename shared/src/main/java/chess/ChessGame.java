6a68157d7a4644d332692b774bc3f4d40c0415b3
James Davies <77803980+jmdav@users.noreply.github.com>
2025-10-02 23:14:31 -0600

    equals and hash and get/set board implemented, pass tests

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -153,7 +154,7 @@ public class ChessGame {
    * @param board the new board to use
    */
   public void setBoard(ChessBoard board) {
-    throw new RuntimeException("Not implemented");
+    this.board = board;
   }
 
   /**
@@ -162,6 +163,20 @@ public class ChessGame {
    * @return the chessboard
    */
   public ChessBoard getBoard() {
-    throw new RuntimeException("Not implemented");
+    return board;
   }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return teamTurn == chessGame.teamTurn && Objects.equals(board, chessGame.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(teamTurn, board);
+    }
 }
