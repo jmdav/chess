92f57374c00b7021d6ea3af6a7c0e91a8f8b0642
James Davies <77803980+jmdav@users.noreply.github.com>
2025-10-22 15:20:31 -0600

    Getting started structure

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,18 @@
 import chess.*;
+import server.Server;
+import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
+        Server server = new Server();
+        server.run(8080);
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        System.out.println("♕ 240 Chess Server: " + generateToken());
     }
+
+    public static String generateToken(){
+        return UUID.randomUUID().toString();
+    }
+
 }
+
